49a00bf8fb0a67920a9cc7c5f96d5de84e31b382
Danny Parnella <danny@launchpadlab.com>
2021-05-17 18:27:05 -0500

    run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_223350) do
+ActiveRecord::Schema.define(version: 2021_05_17_232140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "cc_number"
+    t.date "cc_expiration"
+    t.float "total_cost"
+    t.bigint "showtime_id", null: false
+    t.bigint "customer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+    t.index ["showtime_id"], name: "index_orders_on_showtime_id"
+  end
+
+  create_table "showtimes", force: :cascade do |t|
+    t.datetime "date_time"
+    t.integer "tickets_available"
+    t.float "cost"
+    t.bigint "movie_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_showtimes_on_movie_id"
+  end
 
   create_table "versions", force: :cascade do |t|
     t.string "item_type", null: false
@@ -25,4 +61,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_223350) do
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
+  add_foreign_key "orders", "customers"
+  add_foreign_key "orders", "showtimes"
+  add_foreign_key "showtimes", "movies"
 end
