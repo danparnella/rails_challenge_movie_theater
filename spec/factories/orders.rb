9bfc77c37ba32ad980c3d46c4d6e3864eedbf674
Danny Parnella <danny@launchpadlab.com>
2021-06-24 18:29:03 -0500

    add seeds, update factories to use real date/times

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :order do
     quantity { 1 }
     cc_number { CreditCardValidations::Factory.random(:visa) }
-    cc_expiration { "2025-05-01" }
+    cc_expiration { "05/2025".to_date }
     cc_security_code { "123" }
     total_cost { 49.50 }
     showtime
