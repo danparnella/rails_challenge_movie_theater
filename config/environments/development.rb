6d71e6314b33613005c43777a4b2bfcb3d0b28be
Danny Parnella <danny@launchpadlab.com>
2021-06-29 01:13:18 -0500

    change database time zone, remove duplicate config setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV['APPLICATION_ROOT_URL'] }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
@@ -59,11 +60,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
