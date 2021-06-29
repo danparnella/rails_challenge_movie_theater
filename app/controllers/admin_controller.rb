c73902bdcc971db1a62b40bfc0d86b8d04be4121
Danny Parnella <danny@launchpadlab.com>
2021-06-28 19:06:45 -0500

    move days into a frozen variable

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,10 +1,12 @@
 class AdminController < ApplicationController
+  DAYS_OF_THE_WEEK = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday].freeze
+
   def summary
     if Order.all.any?
       @popular_movies = ordered_showtimes.map(&:movie).uniq[0..2]
       @popular_times = ordered_times.uniq[0..2]
       @total_revenue = Order.all.map(&:total_cost).sum
-      @daily_total_revenues = days_of_the_week.map { |day| day_with_revenue(day) }
+      @daily_total_revenues = DAYS_OF_THE_WEEK.map { |day| day_with_revenue(day) }
     else
       @no_orders = true
     end
@@ -27,10 +29,6 @@ class AdminController < ApplicationController
 
     def ordered_date_times
       ordered_showtimes.pluck(:date_time)
-    end
-
-    def days_of_the_week
-      ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
     end
 
     def day_with_revenue(day)
